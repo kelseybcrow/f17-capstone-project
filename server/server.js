fe2a1860450c8a333964b6c2cc7657acd6f44d08
Kelsey Crow <kelseybcrow@gmail.com>
2022-02-16 22:02:25 -0700

    deploying server

---
+++ server/server.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const cors = require('cors')
+const path = require('path')
+
+const app = express()
+
+app.use(express.json())
+app.use(cors())
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/index.html'))
+})
+
+const port = process.env.PORT || 4000
+
+app.listen(port, () => console.log(`server running on ${port}`))
